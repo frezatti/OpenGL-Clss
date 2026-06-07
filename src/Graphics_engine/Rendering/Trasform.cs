c067923911ee66c626e283a0214ec5649749c424
frezatti <68041159+frezatti@users.noreply.github.com>
2026-06-07 20:11:10 -0300

    Upgrade Transform rotation to Vector3

--- src/Graphics_engine/Rendering/Trasform.cs
+++ src/Graphics_engine/Rendering/Trasform.cs
@@ -6,5 +6,5 @@ public class Transform
 {
     public Vector3 Position { get; set; } = Vector3.Zero;
     public Vector3 Scale { get; set; } = Vector3.One;
-    public float Rotation { get; set; } = 0.0f;
+    public Vector3 Rotation { get; set; } = Vector3.Zero;
 }
