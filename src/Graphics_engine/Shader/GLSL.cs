c1a6d3d004be2c165c268141cd8fa52b51d1cf9f
frezatti <68041159+frezatti@users.noreply.github.com>
2026-06-07 20:13:27 -0300

    Use model view projection shader uniforms

--- src/Graphics_engine/Shader/GLSL.cs
+++ src/Graphics_engine/Shader/GLSL.cs
@@ -8,13 +8,15 @@ public static class GLSL
                 layout (location = 0) in vec3 aPosition; 
                 layout (location = 1) in vec3 aColor; 
 
-                uniform mat4 transform;
+                uniform mat4 model;
+                uniform mat4 view;
+                uniform mat4 projection;
 
                 out vec3 vertexColor;
 
                 void main()
                 {
-                    gl_Position = vec4(aPosition, 1.0) * transform;
+                    gl_Position = projection * view * model * vec4(aPosition, 1.0);
                     vertexColor = aColor;
                 }";
 
