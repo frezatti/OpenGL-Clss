a6ad49cbd3060421a5ed07de98ea6bf2e333e918
frezatti <68041159+frezatti@users.noreply.github.com>
2026-06-08 02:10:55 -0300

    Add normal data constants to mesh

--- src/Graphics_engine/Rendering/Mesh.cs
+++ src/Graphics_engine/Rendering/Mesh.cs
@@ -2,6 +2,15 @@ namespace Graphics_engine;
 
 public class Mesh
 {
+    public const int PositionFloatCount = 3;
+    public const int ColorFloatCount = 3;
+    public const int NormalFloatCount = 3;
+
+    public const int PositionOffset = 0;
+    public const int ColorOffset = PositionOffset + PositionFloatCount;
+    public const int NormalOffset = ColorOffset + ColorFloatCount;
+    public const int FloatsPerVertex = PositionFloatCount + ColorFloatCount + NormalFloatCount;
+
     public float[] Vertice_Data { get; set; } = [];
     public int Vertex_Count { get; set; }
     public Bounds3D Bounds { get; set; }
