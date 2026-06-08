5cb4880fb54be7417bba6f25e4536a32297b7f5c
frezatti <68041159+frezatti@users.noreply.github.com>
2026-06-08 02:47:44 -0300

    Add UV data constants to mesh

--- src/Graphics_engine/Rendering/Mesh.cs
+++ src/Graphics_engine/Rendering/Mesh.cs
@@ -5,11 +5,13 @@ public class Mesh
     public const int PositionFloatCount = 3;
     public const int ColorFloatCount = 3;
     public const int NormalFloatCount = 3;
+    public const int TextureCoordinateFloatCount = 2;
 
     public const int PositionOffset = 0;
     public const int ColorOffset = PositionOffset + PositionFloatCount;
     public const int NormalOffset = ColorOffset + ColorFloatCount;
-    public const int FloatsPerVertex = PositionFloatCount + ColorFloatCount + NormalFloatCount;
+    public const int TextureCoordinateOffset = NormalOffset + NormalFloatCount;
+    public const int FloatsPerVertex = PositionFloatCount + ColorFloatCount + NormalFloatCount + TextureCoordinateFloatCount;
 
     public float[] Vertice_Data { get; set; } = [];
     public int Vertex_Count { get; set; }
